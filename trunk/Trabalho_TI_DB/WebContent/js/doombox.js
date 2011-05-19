8784b71075e679756b7e1f8f57dc476362bf5abc
a.argeu@gmail.com <a.argeu@gmail.com@5ceebd35-bae7-4784-b68a-059fed598a4d>
2011-05-19 23:07:39 +0000

    aletrados os estilos e o plano de fundo

--- trunk/Trabalho_TI_DB/WebContent/js/doombox.js
+++ trunk/Trabalho_TI_DB/WebContent/js/doombox.js
@@ -46,10 +46,10 @@ function radioDiv(){
 	var divDir=document.getElementById('diretorioDiv');
 	var radio=document.getElementById('radio');
 	if(radio.checked==true){
-		divArq.setAttribute("style", 'position:absolute; left:100px; top:350px; visibility: visible;');
+		divArq.setAttribute("style", 'border: ridge; border-color: #aba9a6; border-width: 5px; background-color: #FFFFFF; position:absolute; left:100px; top:390px; visibility: visible;');
 		divDir.setAttribute("style", 'position:absolute; left:100px; top:350px; visibility: hidden;');
 	}else{
-		divDir.setAttribute("style", 'position:absolute; left:100px; top:350px; visibility: visible;');
+		divDir.setAttribute("style", 'border: ridge; border-color: #aba9a6; border-width: 5px; background-color: #FFFFFF; position:absolute; left:100px; top:390px; visibility: visible;');
 		divArq.setAttribute("style", 'position:absolute; left:100px; top:350px; visibility: hidden;');
 	}
 }
--- trunk/Trabalho_TI_DB/WebContent/pages/busca.jsp
+++ trunk/Trabalho_TI_DB/WebContent/pages/busca.jsp
@@ -7,10 +7,12 @@
 <%@page isELIgnored="false"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html:form action="Usuario.do?method=buscarArquivos">
+<div style="border: ridge; border-color: #aba9a6; border-width: 5px; background-color: #FFFFFF; position:absolute; left:57px; top:50px;">
 <table>
 <tr>
 <td>Nome do arquivo ou pasta</td>
-<td><html:text property="nome"/> <html:submit value="OK" /></td>
+<td><html:text property="nome" style="border: ridge; border-color: #000000; border-width: 1px;" /> <html:submit value="OK" /></td>
 </tr>
 </table>
+</div>
 </html:form>
